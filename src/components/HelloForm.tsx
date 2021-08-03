8f748b66baec6780f104c98bd993d0be423f23c0
Phat <ntphat2@orangetheory.com>
2021-08-03 09:56:23 +0700

    first commit

--- src/components/HelloForm.tsx
+++ src/components/HelloForm.tsx
@@ -1,18 +1,19 @@
 import { useForm } from "react-hook-form";
-import { Login } from "types/auth.type";
+import { Hello } from "types/hello.type";
 
 interface Props {}
 
 const HelloForm: React.FC<Props> = () => {
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data: Login) => console.log(data)
+  const onSubmit = (data: Hello) => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" {...register('name')} /> <br />
       <input type="email" {...register('email')} /> <br />
       <button type="submit">Submit</button>
+      <small>Result below developer console</small>
     </form>
   );
 };
